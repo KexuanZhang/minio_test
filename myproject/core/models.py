ba612e3f718df1c634ab153bb809cef71019db23
yiju-zhao <29761446+yiju-zhao@users.noreply.github.com>
2025-07-09 23:52:14 -0400

    fix minio connection issue

--- myproject/core/models.py
+++ myproject/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
+from storages.backends.s3boto3 import S3Boto3Storage
 
 class Document(models.Model):
     name = models.CharField(max_length=100)
-    file = models.FileField()
+    file = models.FileField(upload_to='uploads/', storage=S3Boto3Storage())
